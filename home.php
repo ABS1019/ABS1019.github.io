c1e044edabc0118dd273df15b460f559958dd1bc
Aron Buckley-Smith <A.Buckley-Smith@amaze.com>
2015-11-03 11:15:59 +0000

    padding added to carousel - images added, need to fix

--- home.php
+++ home.php
@@ -1,57 +1,65 @@
 <?php $page="news"; include 'site/header.html';?>
-
-<div>
-	
+<!-- div container for carousel -->
+<div class="container carousel-boxsize">
+	<div class="row">
+		<div class="col-md-12">
+			<!-- Carousel indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#main-carousel" data-slide-to="0" class="active"></li>
+				<li data-target="#main-carousel" data-slide-to="1"></li>
+				<li data-target="#main-carousel" data-slide-to="2"></li>
+				<li data-target="#main-carousel" data-slide-to="3"></li>
+				<li data-target="#main-carousel" data-slide-to="4"></li>		
+			</ol>
+			<!-- Carousel wrappers -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="images/carousel/riccardo.JPG" alt="slide1">
+					
+				<!-- Add picture here along with some text or a button -->
+			
+				</div>
+				<div class="item">
+					<img src="images/carousel/botas.jpg" alt="slide2">
+					
+				<!-- Add picture here along with some text or a button -->
+			
+				</div>
+				<div class="item">
+					<img src="images/carousel/ericsson.jpg" alt="slide3">
+					
+				<!-- Add picture here along with some text or a button -->
+			
+				</div>
+				<div class="item">
+					<img src="images/carousel/ferrari_silverstone.jpg" alt="slide4">
+					
+				<!-- Add picture here along with some text or a button -->
+				
+				</div>
+				<div class="item">
+					<img src="images/carousel/vettel.jpg" alt="slide5">
+					
+				<!-- Add picture here along with some text or a button -->
+			
+				</div>
+			</div>
+		</div>
+		<!-- Icon for carousel controls -->
+		<a href="#main-carousel" class="carousel-control left" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+		</a>
+		<a href="#main-carousel" class="carousel-control right" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+		</a>
+	</div>
 </div>
-<!-- Carousel indicators -->
-<ol class="carousel-indicators">
-	<li data-target="#main-carousel" data-slide-to="0" class="active"></li>
-	<li data-target="#main-carousel" data-slide-to="1"></li>
-	<li data-target="#main-carousel" data-slide-to="2"></li>
-	<li data-target="#main-carousel" data-slide-to="3"></li>
-	<li data-target="#main-carousel" data-slide-to="4"></li>		
-</ol>
-
-<!-- Carousel wrappers -->
-<div class="carousel-inner">
-	<div class="active item">
-		<img src="/carousel/alonso.jpg" alt="Slide 1">
-		
-<!-- Add picture here along with some text or a button -->
-
-	</div>
-	<div class="active item">
-		<img src="/carousel/botas.jpg" alt="Slide 2">
-		
-<!-- Add picture here along with some text or a button -->
-
-	</div>
-	<div class="active item">
-		<img src="/carousel/ericsson.jpg" alt="Slide 3">
-		
-<!-- Add picture here along with some text or a button -->
-
-	</div>
-	<div class="active item">
-		<img src="/carousel/ferrari_silverstone.jpg" alt="Slide 4">
-		
-<!-- Add picture here along with some text or a button -->
-	
-	</div>
-	<div class="active item">
-		<img src="/carousel/riccardo.jpg" alt="Slide 5">
-		
-<!-- Add picture here along with some text or a button -->
-	
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			
+		</div>
 	</div>
 </div>
-
-<!-- Icon for carousel controls -->
-<a href="#main-carousel" class="carousel-control left" data-slide="prev">
-	<span class="glyphicon glyphicon-chevron-left"></span>
-</a>
-<a href="#main-carousel" class="carousel-control right" data-slide="next">
-	<span class="glyphicon glyphicon-chevron-right"></span>
-</a>
 
 <?php include 'site/footer.html';?>
