05b8ede0de2c97d284c7a7b99cfe114f4fd4087d
Aron Buckley-Smith <A.Buckley-Smith@amaze.com>
2015-11-10 11:58:06 +0000

    border and outlining added

--- teams.php
+++ teams.php
@@ -3,68 +3,68 @@
 <div class="container">
 	<!-- Row for Mercedes -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 	
 	<!-- Row for Ferrari -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 	
 	<!-- Row for Williams -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 	
 	<!-- Row for Red Bull Racing -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 	
 	<!-- Row for Force India -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 	
 	<!-- Row for Lotus -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 	
 	<!-- Row for Toro Rosso -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 	
 	<!-- Row for Red Bull -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 	
 	<!-- Row for Sauber -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 	
 	<!-- Row for McLaren -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 	
 	<!-- Row for Marussia -->
 	<div class="row">
-		<div class="col-sm-6"></div>
-		<div class="col-sm-6"></div>
+		<div class="col-sm-6 team-info"></div>
+		<div class="col-sm-6 team-img"></div>
 	</div>
 </div>
 
