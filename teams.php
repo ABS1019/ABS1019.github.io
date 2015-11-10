395fb34f9c78ae1cb04f925f8ae0416d03817809
Aron Buckley-Smith <A.Buckley-Smith@amaze.com>
2015-11-10 11:43:17 +0000

    team row blocks added along with images to be used for each team

--- teams.php
+++ teams.php
@@ -1,5 +1,72 @@
 <?php $page="teams"; include 'site/header.html';?>
 
+<div class="container">
+	<!-- Row for Mercedes -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+	
+	<!-- Row for Ferrari -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+	
+	<!-- Row for Williams -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+	
+	<!-- Row for Red Bull Racing -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+	
+	<!-- Row for Force India -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+	
+	<!-- Row for Lotus -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+	
+	<!-- Row for Toro Rosso -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+	
+	<!-- Row for Red Bull -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+	
+	<!-- Row for Sauber -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+	
+	<!-- Row for McLaren -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+	
+	<!-- Row for Marussia -->
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6"></div>
+	</div>
+</div>
 
 
 <?php include 'site/footer.html';?>
