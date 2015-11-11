cbc10553c3733a2993dc458d627a9317fa9aaf6c
Aron Buckley-Smith <A.Buckley-Smith@amaze.com>
2015-11-11 13:18:19 +0000

    photos updated, transitions applied to info tables, more CSS added for the whole page

--- teams.php
+++ teams.php
@@ -1,69 +1,89 @@
 <?php $page="teams"; include 'site/header.html';?>
 
-<div class="container team-width">
+<div class="container-fluid team-width">
 	<!-- Page title and subheading -->
 	<div class="row teams-header">
 		<h1>Championship Teams<small>All Teams</small></h1>
 	</div>
 	
 	<!-- Row for Mercedes -->
-	<div class="row">
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-info" style="border-right: 5px solid #2AB4A5"></div>
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-img"></div>
+	<div class="row no-gutter">
+		<div class="col-sm-6 team-info" style="border-right: 5px solid #2AB4A5"></div>
+		<div class="col-sm-6" style="padding: 0;">
+			<img class="team-img img-grow" src="images/teams/merc.jpg" alt="Mercedes Photo">
+		</div>
 	</div>
 	
 	<!-- Row for Ferrari -->
 	<div class="row">
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-info" style="border-right: 5px solid #C30000"></div>
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-img"></div>
+		<div class="col-sm-6 team-info" style="border-right: 5px solid #C30000"></div>
+		<div class="col-sm-6" style="padding: 0;">
+			<img class="team-img img-grow" src="images/teams/enzo.jpg" alt="Ferrari Photo">
+		</div>
 	</div>
 	
 	<!-- Row for Williams -->
 	<div class="row">
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-info" style="border-right: 5px solid #FFFFFF"></div>
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-img"></div>
+		<div class="col-sm-6 team-info" style="border-right: 5px solid #FFFFFF"></div>
+		<div class="col-sm-6" style="padding: 0;">
+			<img class="team-img img-grow" src="images/teams/williams.jpg" alt="Williams Photo">
+		</div>
 	</div>
 	
 	<!-- Row for Red Bull Racing -->
 	<div class="row">
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-info" style="border-right: 5px solid #4F1AAB"></div>
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-img"></div>
+		<div class="col-sm-6 team-info" style="border-right: 5px solid #4F1AAB"></div>
+		<div class="col-sm-6" style="padding: 0;">
+			<img class="team-img img-grow" src="images/teams/rbr.jpg" alt="Red Bull Racing Photo">
+		</div>
 	</div>
 	
 	<!-- Row for Force India -->
 	<div class="row">
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-info" style="border-right: 5px solid #FF5F0F"></div>
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-img"></div>
+		<div class="col-sm-6 team-info" style="border-right: 5px solid #FF5F0F"></div>
+		<div class="col-sm-6" style="padding: 0;">
+			<img class="team-img img-grow" src="images/teams/fi.jpg" alt="Force India Photo">
+		</div>
 	</div>
 	
 	<!-- Row for Lotus -->
 	<div class="row">
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-info" style="border-right: 5px solid #FFB800"></div>
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-img"></div>
+		<div class="col-sm-6 team-info" style="border-right: 5px solid #FFB800"></div>
+		<div class="col-sm-6" style="padding: 0;">
+			<img class="team-img img-grow" src="images/teams/lotus.jpg" alt="Lotus Photo">
+		</div>
 	</div>
 	
 	<!-- Row for Toro Rosso -->
 	<div class="row">
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-info" style="border-right: 5px solid #0005C1"></div>
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-img"></div>
+		<div class="col-sm-6 team-info" style="border-right: 5px solid #0005C1"></div>
+		<div class="col-sm-6" style="padding: 0;">
+			<img class="team-img img-grow" src="images/teams/tr.jpg" alt="Toro Rosso Photo">
+		</div>
 	</div>
 	
 	<!-- Row for Sauber -->
 	<div class="row">
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-info" style="border-right: 5px solid #0063FF"></div>
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-img"></div>
+		<div class="col-sm-6 team-info" style="border-right: 5px solid #0063FF"></div>
+		<div class="col-sm-6" style="padding: 0;">
+			<img class="team-img img-grow" src="images/teams/sauber.jpg" alt="Sauber Photo">
+		</div>
 	</div>
 	
 	<!-- Row for McLaren -->
 	<div class="row">
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-info" style="border-right: 5px solid #808080"></div>
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-img"></div>
+		<div class="col-sm-6 team-info" style="border-right: 5px solid #808080"></div>
+		<div class="col-sm-6" style="padding: 0;">
+			<img class="team-img img-grow" src="images/teams/mclaren.jpg" alt="McLaren Photo">
+		</div>
 	</div>
 	
 	<!-- Row for Marussia -->
 	<div class="row">
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-info" style="border-right: 5px solid #6E0000"></div>
-		<div class="col-sm-6 col-sm-12 col-xs-12 team-img"></div>
+		<div class="col-sm-6 team-info" style="border-right: 5px solid #6E0000"></div>
+		<div class="col-sm-6" style="padding: 0;">
+			<img class="team-img img-grow" src="images/teams/marussia.jpg" alt="Marussia Photo">
+		</div>
 	</div>
 </div>
 
